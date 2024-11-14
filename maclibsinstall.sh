7949b2703adb84aff40d2c82b4be0401f101778d
Ansh rathod <anshrathod29@gmail.com>
2024-11-14 14:47:22 +0530

    success test

--- maclibsinstall.sh
+++ maclibsinstall.sh
@@ -15,6 +15,16 @@ echo -e "\nBoth ffmpeg and ImageMagick are already installed.\n"
 
 sleep 5
 
+FFMPEG_PATH=$(which ffmpeg)
+IMAGEMAGICK_PATH=$(which magick)
 
-echo "Error: An error occurred during one or more installations."
+# Print the paths with some formatting
+echo -e "\nPaths for installed tools:"
+echo -e "---------------------------------"
+echo -e "FFmpeg path: $FFMPEG_PATH"
+echo -e "ImageMagick path: $IMAGEMAGICK_PATH"
+echo -e "---------------------------------\n"
+
+
+echo "All checks and installations are completed successfully."
 
