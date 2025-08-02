0fce34725c36fdb121dc183464e5847feede3e2a
Ansh Rathod <anshrathod29@gmail.com>
2025-08-02 15:57:49 +0530

    update for livecheck

--- Casks/pimosa.rb
+++ Casks/pimosa.rb
@@ -1,11 +1,17 @@
 cask "pimosa" do
-  version "v1.3.0+26"
+  version "1.3.0,26"
   sha256 "dea4b99afed7695ea78ea4ba4d46d2e604bb0e744ae5f5732e2ac6197d97677a"
 
-  url "https://github.com/Ansh-Rathod/pimosa-builds/releases/download/v1.3.0%2B26/Pimosa.zip"
+  url "https://github.com/Ansh-Rathod/pimosa-builds/releases/download/v#{version.before_comma}+#{version.after_comma}/Pimosa.zip",
+      verified: "github.com/Ansh-Rathod/pimosa-builds/"
   name "Pimosa"
   desc "Offline media toolkit for video, audio, and images"
   homepage "https://pimosa.app"
+
+  livecheck do
+    url "https://github.com/Ansh-Rathod/pimosa-builds"
+    strategy :github_latest
+  end
 
   app "Pimosa.app"
 
